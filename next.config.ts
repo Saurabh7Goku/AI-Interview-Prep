6d1e948b2300e6736455f5748d9367aca1e88c3a
Saurabh Singh <saurabhgk7@gmail.com>
2025-07-23 16:52:43 +0530

    bug: pdf-parse test file location error, Resolved

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
 import type { NextConfig } from "next";
+const path = require('path');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config: { resolve: { alias: { [x: string]: any; }; }; }, { isServer }: any) => {
+    if (isServer) {
+      config.resolve.alias['pdf-parse'] = path.resolve(
+        process.cwd(),
+        'node_modules/pdf-parse/lib/pdf-parse.js'
+      );
+    }
+    return config;
+  },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: process.env.NODE_ENV === 'production',
   },
 };
 
